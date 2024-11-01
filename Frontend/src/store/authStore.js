25e5aae82e635e2dfb5e3b13b4dd678240c6ae54
dpvasani <vadanidarshan@gmail.com>
2024-11-01 16:21:11 +0530

    Deployment Setup

--- Frontend/src/store/authStore.js
+++ Frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "https://advancedauth-frontend.vercel.app/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "https://advancedauth-ws2g.onrender.com/api/auth";
 
 axios.defaults.withCredentials = true;
 
